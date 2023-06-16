f476d6f55044781e497a1548ad63737c16f1d0db
kuzu2000 <swan.t.zaw@gmail.com>
2023-06-16 18:18:23 +0630

    https

--- ecommerce-frontend/src/redux/requestMethod.js
+++ ecommerce-frontend/src/redux/requestMethod.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://electronic-ecommerce-api.vercel.app/api';
+const BASE_URL = 'https://electronic-ecommerce-api.vercel.app/api';
 
 const user = JSON.parse(localStorage.getItem('persist:root'))?.user;
 const currentUser = user && JSON.parse(user).currentUser;
